79e907ef506157152ef332c6736e531f656ca96a
Michael Ma <michaelma@httpeace.com>
2018-02-11 23:49:12 +0800

    first draft version for nextjs

---
+++ pages/about.js
@@ -0,0 +1,7 @@
+import Layout from '../components/layouts'
+
+export default () => (
+  <Layout>
+    <p>This is the about page</p>
+  </Layout>
+)
---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import Layout from '../components/layouts'
+
+const Index = () => (
+  <Layout>
+    <p>This is homepage</p>
+  </Layout>
+)
+
+export default Index
